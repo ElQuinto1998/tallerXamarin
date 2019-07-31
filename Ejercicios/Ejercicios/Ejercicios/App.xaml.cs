12ea0b918f18aff352af6e9b35e57d7ed8ca4072
Andres Quinto Quinto <andrquqn@suramericana.com.co>
2019-07-31 18:34:55 -0500

    Se suben trabajos

---
+++ Ejercicios/Ejercicios/Ejercicios/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Ejercicios.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Ejercicios
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new InstagramPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
